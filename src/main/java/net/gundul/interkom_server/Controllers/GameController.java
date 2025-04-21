03b41d3085422fd6020d149b25c871f1d06d8367
Gundul <sampah@mailbox.org>
2025-04-21 10:59:14 +0200

    add simple cron example

--- src/main/java/net/gundul/interkom_server/Controllers/GameController.java
+++ src/main/java/net/gundul/interkom_server/Controllers/GameController.java
@@ -8,15 +8,17 @@ import net.gundul.interkom_server.Services.PlayerService;
 import org.apache.tomcat.util.json.ParseException;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/game")
@@ -120,4 +122,11 @@ public class GameController {
 
 		return new ResponseEntity<String>(player, HttpStatus.OK);
 	}
+
+	@Scheduled(cron = "30 * * * * ?")
+	public void testCron()
+	{
+		System.out.println(">>> YEAH, hi from Mr. Cron <<<");
+	}
+
 }
--- src/main/java/net/gundul/interkom_server/InterkomServerApplication.java
+++ src/main/java/net/gundul/interkom_server/InterkomServerApplication.java
@@ -3,10 +3,12 @@ package net.gundul.interkom_server;
 import Utils.Security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableScheduling
 public class InterkomServerApplication
 {
 	public static void main(String[] args)
