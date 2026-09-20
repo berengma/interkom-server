0369b8cd3cfe0abb21648025124854af39ab971a
Gundul <sampah@mailbox.org>
2026-09-20 17:29:19 +0200

    adjust server URI

--- src/main/java/net/gundul/interkom_server/config/WebConfig.java
+++ src/main/java/net/gundul/interkom_server/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // Allow CORS for all endpoints
-				.allowedOrigins("http://localhost:30099") // Adjust this to your frontend's origin
+				.allowedOrigins("http://lilly,gundul.net:30099") // Adjust this to your frontend's origin
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
 				.allowCredentials(true);
