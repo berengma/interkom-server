20586cfb1367121eb6865d835be112b05b8b9b8a
Gundul <sampah@mailbox.org>
2026-09-20 19:42:05 +0200

    make allowedOrigin configurable by properties and env

--- src/main/java/net/gundul/interkom_server/config/WebConfig.java
+++ src/main/java/net/gundul/interkom_server/config/WebConfig.java
@@ -1,17 +1,27 @@
 package net.gundul.interkom_server.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer
+{
+	private final String frontendOrigin;
+
+	public WebConfig(
+			@Value("${app.cors.frontend-uri}") String frontendUri,
+			@Value("${app.cors.frontend-port}") String frontendPort
+	)
+	{
+		this.frontendOrigin = frontendUri + ':' + frontendPort;
+	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // Allow CORS for all endpoints
-				.allowedOrigins("http://lilly.gundul.net:30099") // Adjust this to your frontend's origin
+				.allowedOrigins(frontendOrigin)
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
 				.allowCredentials(true);
