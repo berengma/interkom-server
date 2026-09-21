c77c90d1e372c89db877a3dc756877fe4133018c
Gundul <sampah@mailbox.org>
2026-09-21 18:54:46 +0200

    fix dynamic URI and PORT with CARS issue

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -1,3 +1,5 @@
 #!bash
-echo "All setup for Interkom !"
+echo "All setup for Interkom ! ${INTERKOM_URL}:${WEB_PORT}"
+export  INTERKOM_URL
+export  WEB_PORT
 exec java -jar /home/interkom/interkom-server/target/interkom-server-0.0.2-MVP.jar
